9f64055e4f8047a1449293f73e57a403c29a3723
garretthauserCUB <gaha9669@colorado.edu>
2025-09-14 13:45:20 -0600

    update get_column in my_utils.py to return int values

--- my_utils.py
+++ my_utils.py
@@ -9,6 +9,6 @@ def get_column(file_name, query_column, query_value, result_column=1):
             if type(result_column) is str:
                 result_column = header.index(result_column)
         if row[query_column] == query_value:
-            output.append(row[result_column])
+            output.append(int(float(row[result_column])))
     f.close()
     return output
