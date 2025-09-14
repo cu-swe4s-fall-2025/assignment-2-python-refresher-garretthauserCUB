14124dd9afdc0122774fe007d7c82fd992cdf5b7
garretthauserCUB <gaha9669@colorado.edu>
2025-09-14 13:44:32 -0600

    update print_fires.py to take arguments form command line with argparse. update run.sh to include arguments

--- print_fires.py
+++ print_fires.py
@@ -1,9 +1,35 @@
 import my_utils as mu
+import argparse
 
-country = 'Afghanistan'
-county_column = 0
-forestFires_column = 'Forest fires'
-file_name = 'Agrofood_co2_emission.csv'
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--file_name',
+                    type=str,
+                    required=True,
+                    help='CSV file name')
+parser.add_argument('--county_column',
+                    type=int,
+                    required=True,
+                    help='Index of the country column')
+parser.add_argument('--forestFires_column',
+                    type=str,
+                    required=True,
+                    help='Name of the column')
+parser.add_argument('--country',
+                    type=str,
+                    required=True,
+                    help='Country name')
+
+args = parser.parse_args()
+
+file_name = args.file_name
+county_column = args.county_column
+country = args.country
+forestFires_column = args.forestFires_column
+
+if county_column != 0:
+    raise ValueError("county_column must be 0")
+
 forestFires = mu.get_column(file_name,
                             county_column,
                             country,
